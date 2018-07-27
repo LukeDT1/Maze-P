1d707de10de8d42f0d2915f8778f7f0bcb872914
Arius <arius@filtered.ai>
2018-07-27 11:40:07 -0400

    COLLOSION sruff
    collision stuff for capsile

--- Assets/platercontroller.cs
+++ Assets/platercontroller.cs
@@ -12,21 +12,23 @@ public class platercontroller : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(0, 0, .25f);
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(0, 0, -.25f);
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(0, -turnSpeed, 0);
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(0, turnSpeed, 0);
+		if (Input.GetKey (KeyCode.UpArrow))
+			transform.Translate (0, 0, .25f);
+		if (Input.GetKey (KeyCode.DownArrow))
+			transform.Translate (0, 0, -.25f);
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Rotate (0, -turnSpeed, 0);
+		if (Input.GetKey (KeyCode.RightArrow))
+			transform.Rotate (0, turnSpeed, 0);
        
     
+	}
 
+	void OnCollisionEnter (Collision col) {
+		playerRigidbody.velocity = Vector3.zero;
+	}
 
-            
-            
-
+	void FixedUpdate () {
         playerRigidbody.velocity = Vector3.zero;
     }
      

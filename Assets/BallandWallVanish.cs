c92c83d856f8b50601cf85877c483ce0563ed94f
Arius <arius@filtered.ai>
2018-07-27 11:40:28 -0400

    Disappearance act

---
+++ Assets/BallandWallVanish.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallandWallVanish : MonoBehaviour {
+	public GameObject wall;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void OnTriggerEnter(Collider other){
+	
+		if(other.gameObject.name == "Player")
+		{
+			Destroy (gameObject);
+			Destroy (wall);
+		}
+	
+	}
+}

48c08bd27160bcd0fdc70e193d77d0ad00a7c748
Arius <arius@filtered.ai>
2018-07-27 13:23:27 -0400

    Adds movement overhaul and tries to make you win appear

---
+++ Assets/YouWinAppear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouWinAppear : MonoBehaviour {
+	public GameObject canvas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void OnTriggerEnter(Collider other){
+
+		if(other.gameObject.name == "Player")
+		{
+			Destroy (gameObject);
+			gameObject.SetActive(canvas);
+		}
+	}
+}
